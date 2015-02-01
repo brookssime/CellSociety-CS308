9487db4c18cdbc15380fada853d4daeed7ece20e
James Mosca <jcm73@duke.edu>
2015-01-31 22:10:35 -0500

    add beginnings of xml files

--- src/CellSocietyView.java
+++ src/CellSocietyView.java
@@ -13,7 +13,7 @@ import javafx.util.Duration;
 
 
 public class CellSocietyView {
-    private static final String DEFAULT_RESOURCE_PACKAGE = "resources/";
+    public static final String DEFAULT_RESOURCE_PACKAGE = "resources/";
 	
 	private CellularAutomata myCA;
 	private GridDrawer myGridView;
