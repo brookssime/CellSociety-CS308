3a1668f176d235ed1870dd1458f0eb1d4b231985
Brooks Sime <brookssime@gmail.com>
2015-01-31 22:31:45 -0500

    t

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,13 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-<<<<<<< HEAD
+
 	private int FRAME_TIME = 10000;
 	private CellularAutomata myCA;
 	
 	
-	
-=======
-	private int FRAME_TIME = 2000;
 
->>>>>>> a2ce82a45356942ef319ccb45037e13e27a04392
+
 	@Override
 	public void start(Stage s) throws Exception {
 
